85cba4c892bb2728e61b180d0343f2537a5ecc19
sandyabhi <abhisandy47@gmail.com>
2022-10-05 13:50:00 +0530

    initial commit

---
+++ hardhat-NFT/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x4aB0EAddc8374bA526E33d0334A934D32478ed3D";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
